c9b2d3943c90ae89d8979c6a3c568bd11d20f7c3
Aman Yadav <yadavaman150902@gmail.com>
2024-07-27 23:58:28 +0530

    Day-11 Solution

---
+++ Day-11-Mathmatical/462. Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int start = 0;
+        int end = nums.size()-1;
+        int count = 0;
+        
+        while(start<end)
+        {
+            count += abs(nums[end]-nums[start]);
+            start++;
+            end--;
+        }
+    return count;
+    }
+};
