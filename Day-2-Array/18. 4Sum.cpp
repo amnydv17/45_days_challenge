4717e3801429eae7cdf45885ad735c122256cbca
Aman Yadav <yadavaman150902@gmail.com>
2024-08-13 22:05:06 +0530

    Updated

--- Day-2-Array/18. 4Sum.cpp
+++ Day-2-Array/18. 4Sum.cpp
@@ -4,16 +4,16 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         int n = nums.size();
-        for(int i = 0; i< n - 3; i++){
+        for(int i = 0; i < n - 3; i++){
             if(i > 0 && nums[i] == nums[i - 1]){
                 continue;
             }
-            for(int j = i + 1; j < n-2; j++){
+            for(int j = i + 1; j < n - 2; j++){
                 if(j > i + 1 && nums[j] == nums[j - 1]){
                     continue;
                 }
                 
-                int tSum = target - (nums[i] + nums[j]);
+                long long tSum = (long long)target - (long long)(nums[i] + nums[j]);
                 int l = j + 1, h = n - 1;
                 while (l < h){
                     if (nums[l] + nums[h] == tSum){
@@ -34,12 +34,9 @@ public:
                     else{
                         h--;
                     }
-                    
                 }
-                
             }
         }
-          
         return ans;
     }
 };
